857262c52ef4b935c6bf62094dcc265e032c108d
TangRuijie <thss15_tangrj@163.com>
2017-12-26 08:29:57 -0800

    PC GUI

--- PC/oscilloscope.py
+++ PC/oscilloscope.py
@@ -8,16 +8,17 @@ import tos
 
 AM_OSCILLOSCOPE = 0x30
 am = tos.AM()
+stream_data = ['0', '0', '0', '0', '0', '0']
 
 class OscilloscopeMsg(tos.Packet):
     def __init__(self, packet = None):
         tos.Packet.__init__(self,
-                            [('id', 'int', 16),
-                             ('seq', 'int', 16),
-                             ('humidity', 'int', 16),
-                             ('light', 'int', 16),
-                             ('temperature', 'int', 16),
-                             ('timestamp', 'int', 32)],
+                            [('id', 'int', 2),
+                             ('seq', 'int', 2),
+                             ('humidity', 'int', 2),
+                             ('light', 'int', 2),
+                             ('temperature', 'int', 2),
+                             ('timestamp', 'int', 4)],
                             packet)
 
 class BasePlot(object):
@@ -40,8 +41,7 @@ class BasePlot(object):
         self.app.exit()
 
     def plot_init(self):
-        trial_data = ['0', '0', '0']
-        for i in range(len(trial_data)):
+        for i in range(len(stream_data)):
             new_plot = self.layout.addPlot()
             new_plot.plot(np.zeros(250))
             self.plot_list.append(new_plot.listDataItems()[0])
@@ -51,20 +51,24 @@ class BasePlot(object):
         p = am.read()
         if p and p.type == AM_OSCILLOSCOPE:
             msg = OscilloscopeMsg(p.data)
-            stream_data = []
-            stream_data.append(str(msg.temperature))
-            stream_data.append(str(msg.humidity))
-            stream_data.append(str(msg.light))
-            for data, line in zip(stream_data, self.plot_list):
-                line.informViewBoundsChanged()
-                line.xData = np.arange(len(line.yData))
-                line.yData = np.roll(line.yData, -1)
-                line.yData[-1] = data
-                line.xClean = line.yClean = None
-                line.xDisp = None
-                line.yDisp = None
-                line.updateItems()
-                line.sigPlotChanged.emit(line)
+            if (msg.id == 100):
+                stream_data[0] = str(msg.temperature)
+                stream_data[1] = str(msg.humidity)
+                stream_data[2] = str(msg.light)
+            else:
+                stream_data[3] = str(msg.temperature)
+                stream_data[4] = str(msg.humidity)
+                stream_data[5] = str(msg.light)
+        for data, line in zip(stream_data, self.plot_list):
+            line.informViewBoundsChanged()
+            line.xData = np.arange(len(line.yData))
+            line.yData = np.roll(line.yData, -1)
+            line.yData[-1] = data
+            line.xClean = line.yClean = None
+            line.xDisp = None
+            line.yDisp = None
+            line.updateItems()
+            line.sigPlotChanged.emit(line)
  
     def start(self):
         self.plot_init()
